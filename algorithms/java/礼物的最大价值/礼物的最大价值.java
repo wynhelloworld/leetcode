7526652cc50b951088f1a918cf2b2533c1443093
bytenan <bytenan@outlook.com>
2023-06-15 21:11:01 +0800

    Added a java solution for problem 剑指Offer 47

---
+++ algorithms/java/礼物的最大价值/礼物的最大价值.java
@@ -0,0 +1,17 @@
+// Source : https://leetcode.cn/problems/li-wu-de-zui-da-jie-zhi-lcof/
+// Author : Yanan Wang
+// Date   : 2023-06-15
+
+class Solution {
+    public int maxValue(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+            }
+        }
+        return dp[m][n];
+    }
+}
