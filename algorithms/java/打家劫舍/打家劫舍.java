96d91de508af1569f0b2dd4b25e0277418fa4e23
bytenan <bytenan@outlook.com>
2023-06-18 22:32:10 +0800

    Added a java solution for problem 198

---
+++ algorithms/java/打家劫舍/打家劫舍.java
@@ -0,0 +1,16 @@
+// Source : https://leetcode.cn/problems/house-robber/
+// Author : Yanan Wang
+// Date   : 2023-06-18
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int f[] = new int[n + 1];
+        int g[] = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            f[i] = g[i - 1] + nums[i - 1];
+            g[i] = Math.max(f[i - 1], g[i - 1]);
+        }
+        return Math.max(f[n], g[n]);
+    }
+}
