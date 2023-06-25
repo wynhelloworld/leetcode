e1cc8c76dfd600831e677450cb12214ee902dedf
bytenan <bytenan@outlook.com>
2023-06-25 18:29:14 +0800

    Added a java solution for problem 300

---
+++ algorithms/java/最长递增子序列/最长递增子序列.java
@@ -0,0 +1,23 @@
+// Source : https://leetcode.cn/problems/longest-increasing-subsequence/
+// Author : Yanan Wang
+// Date   : 2023-06-25
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+        int result = 1; 
+        for (int i = 1; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+        return result;
+    }
+}
