d95d759da72371d4e6c9a85c0d845c0bc93a4b92
bytenan <bytenan@outlook.com>
2023-09-07 18:04:04 +0800

    Added a cpp solution for problem 784

---
+++ algorithms/cpp/字母大小写全排列/字母大小写全排列.cpp
@@ -0,0 +1,34 @@
+// Source : https://leetcode.cn/problems/single-number-iii/description/
+// Author : Yanan Wang
+// Date   : 2023-09-07
+
+// 本题先画一颗决策树：当递归到数字时，直接添加上递归到下一层，当递归到字母时，转变大小写进行一次递归，不转变大小写再进行一次递归
+
+class Solution {
+private:
+    vector<string> ans;
+    string path;
+public:
+    void dfs(const string &s, int index) {
+        if (s.size() == index) {
+            ans.push_back(path);
+            return;
+        }
+        if (isalpha(s[index])) {
+            if (isupper(s[index])) {
+                path.push_back(tolower(s[index]));
+            } else {
+                path.push_back(toupper(s[index]));
+            }
+            dfs(s, index + 1);
+            path.pop_back();
+        }
+        path.push_back(s[index]);
+        dfs(s, index + 1);
+        path.pop_back();
+    }
+    vector<string> letterCasePermutation(string s) {
+        dfs(s, 0);
+        return ans;
+    }
+};
