b8ab67425495b403fccc4da59731f81cd90de819
wynhelloworld <wynhelloworld@gmail.com>
2023-11-02 19:19:36 +0800

    Added a cpp solution for problem 695

--- algorithms/cpp/岛屿的最大面积/岛屿的最大面积.cpp
+++ algorithms/cpp/岛屿的最大面积/岛屿的最大面积.cpp
@@ -2,13 +2,14 @@
 // Author : Yanan Wang
 // Date   : 2023-09-15
 
+/* 解法一: DFS */
 class Solution {
-private:
+public:
     int dx[4] = {0, 0, 1, -1};
     int dy[4] = {1, -1, 0, 0};
     bool used[50][50];
     int m, n;
-public:
+    
     int dfs(vector<vector<int>> &grid, int i, int j) {
         int sum = 1;
         used[i][j] = true;
@@ -20,6 +21,7 @@ public:
         }
         return sum;
     }
+
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int ans = 0;
         m = grid.size(), n = grid[0].size();
@@ -31,5 +33,51 @@ public:
             }
         }
         return ans;
+    }
+};
+
+/* 解法二: BFS */
+class Solution {
+public:
+    int dx[4] = {0, 0, -1, 1};
+    int dy[4] = {-1, 1, 0, 0};
+    bool used[50][50] = { false };
+    int m, n;
+    int ans = 0;
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1 && !used[i][j]) {
+                    bfs(grid, i, j);
+                }
+            }
+        }
+        return ans;
+    }
+
+    void bfs(vector<vector<int>> &grid, int i, int j) {
+        queue<pair<int, int>> q;
+        q.push({i, j});
+        used[i][j] = true;
+        
+        int cnt = 0;
+        while (!q.empty()) {
+            ++cnt;
+            auto [a, b] = q.front();
+            q.pop();
+            for (int i = 0; i < 4; ++i) {
+                int x = a + dx[i];
+                int y = b + dy[i];
+                if (x >= 0 && x < m && y >= 0 && y < n && grid[x][y] == 1 && !used[x][y]) {
+                    q.push({x, y});
+                    used[x][y] = true;
+                }
+            }            
+        }
+        ans = max(ans, cnt);
     }
 };
