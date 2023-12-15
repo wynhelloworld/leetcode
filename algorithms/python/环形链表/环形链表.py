34517341bec3e78fd4d306c69bf05d20ec98da3a
wynhelloworld <wynhelloworld@gmail.com>
2023-12-15 21:52:05 +0800

    Added a python solution for problem 141

---
+++ algorithms/python/环形链表/环形链表.py
@@ -0,0 +1,20 @@
+# Source : https://leetcode.cn/problems/linked-list-cycle/description/
+# Author : Yanan Wang
+# Date   : 2023-12-15
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow is fast:
+                return True
+        return False
