375481b4f3fb9952125a5e45e827efb6284f0fcc
wynhelloworld <wynhelloworld@gmail.com>
2023-12-16 19:10:21 +0800

    Added a python solution for problem 92

---
+++ algorithms/python/反转链表 II/反转链表 II.py
@@ -0,0 +1,23 @@
+# Source : https://leetcode.cn/problems/reverse-linked-list-ii/description/
+# Author : Yanan Wang
+# Date   : 2023-12-16
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        pre = dummy = ListNode(-1, head)
+        for _ in range(left - 1):
+            pre = pre.next
+
+        cur = pre.next
+        for _ in range(right - left):
+            next = cur.next
+            cur.next = next.next
+            next.next = pre.next
+            pre.next = next
+
+        return dummy.next    
