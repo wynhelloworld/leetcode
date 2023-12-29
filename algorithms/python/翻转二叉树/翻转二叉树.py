4d2550a3fb4b88d1a5b9ea275af11ebdb96ba6b0
wynhelloworld <wynhelloworld@gmail.com>
2023-12-29 16:33:21 +0800

    Added a python solution for problem 226

---
+++ algorithms/python/翻转二叉树/翻转二叉树.py
@@ -0,0 +1,18 @@
+# Source : https://leetcode.cn/problems/invert-binary-tree/description/
+# Author : Yanan Wang
+# Date   : 2023-12-29
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
