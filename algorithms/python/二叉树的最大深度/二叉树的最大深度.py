ceee696ceb4f622a592956f77d3bc6e156a1f22f
wynhelloworld <wynhelloworld@gmail.com>
2023-12-29 16:32:04 +0800

    Added a python solution for problem 104

---
+++ algorithms/python/二叉树的最大深度/二叉树的最大深度.py
@@ -0,0 +1,17 @@
+# Source : https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/
+# Author : Yanan Wang
+# Date   : 2023-12-29
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        return 1 + max(left_depth, right_depth)
