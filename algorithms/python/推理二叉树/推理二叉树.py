df41cfa76ce3328212623bb71189988850ffc294
wynhelloworld <wynhelloworld@gmail.com>
2023-12-30 19:40:46 +0800

    Added a python solution for problem LCR 124

---
+++ algorithms/python/推理二叉树/推理二叉树.py
@@ -0,0 +1,35 @@
+# Source : https://leetcode.cn/problems/zhong-jian-er-cha-shu-lcof/description/
+# Author : Yanan Wang
+# Date   : 2023-12-30
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deduceTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        valToIndex = {}
+        for idx, val in enumerate(inorder):
+            valToIndex[val] = idx
+        return self.build(preorder, 0, len(preorder) - 1,
+                        inorder, 0, len(inorder) - 1, valToIndex)
+
+    def build(self, preorder, preStart, preEnd, inorder, inStart, inEnd, valToIndex):
+        if preStart > preEnd:
+            return None
+        
+        rootVal = preorder[preStart]
+        rootIdx = valToIndex[rootVal]
+        leftSize = rootIdx - inStart
+        
+        root = TreeNode(rootVal)
+        
+        root.left = self.build(preorder, preStart + 1, preStart + leftSize,
+                            inorder, inStart, rootIdx - 1, valToIndex)
+
+        root.right = self.build(preorder, preStart + 1 + leftSize, preEnd,
+                            inorder, rootIdx + 1, inEnd, valToIndex)
+
+        return root
