44b937c4a10fa59b101933ed3efc402a494cfc48
wynhelloworld <wynhelloworld@gmail.com>
2024-01-15 23:35:53 +0800

    Added a python solution for problem 123

---
+++ algorithms/python/买卖股票的最佳时机 III/买卖股票的最佳时机 III.py
@@ -0,0 +1,24 @@
+# Source : https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iii/
+# Author : Yanan Wang
+# Date   : 2024-01-15
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        f = [[float('-inf')] * 3 for _ in range(n)]
+        g = [[float('-inf')] * 3 for _ in range(n)]
+        f[0][0] = -prices[0]
+        g[0][0] = 0
+        
+        for i in range(1, n):
+            for j in range(3):
+                f[i][j] = max(f[i - 1][j], g[i - 1][j] - prices[i])
+                g[i][j] = g[i - 1][j]
+                if j > 0:
+                    g[i][j] = max(g[i][j], f[i - 1][j - 1] + prices[i])
+
+        ans = 0
+        for j in range(1, 3):
+            ans = max(ans, g[n - 1][j])
+        
+        return ans
