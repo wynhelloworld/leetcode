077d135f58606aedf58959acc032e5fedd089567
wynhelloworld <wynhelloworld@gmail.com>
2024-01-27 17:30:40 +0800

    Added a python solution for problem 44

---
+++ algorithms/python/通配符匹配/通配符匹配.py
@@ -0,0 +1,20 @@
+# Source : https://leetcode.cn/problems/wildcard-matching/description/
+# Author : Yanan Wang
+# Date   : 2024-01-27
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True
+        for j in range(1, n + 1):
+            if p[j - 1] != '*':
+                break
+            dp[0][j] = True
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s[i - 1] == p[j - 1] or p[j - 1] == '?':
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+        return dp[-1][-1]
