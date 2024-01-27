5ba85d284a4fddc2a822d0dbdc28c7ea80f97028
wynhelloworld <wynhelloworld@gmail.com>
2024-01-27 17:30:27 +0800

    Added a python solution for problem 115

---
+++ algorithms/python/不同的子序列/不同的子序列.py
@@ -0,0 +1,16 @@
+# Source : https://leetcode.cn/problems/distinct-subsequences/
+# Author : Yanan Wang
+# Date   : 2024-01-27
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1):
+            dp[i][0] = 1
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = dp[i - 1][j]
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] += dp[i - 1][j - 1]
+        return dp[-1][-1]
